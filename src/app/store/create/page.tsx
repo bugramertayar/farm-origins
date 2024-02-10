4b231ec350e1e8e7b9b4e03cd033efab822f13d8
Buğra Mert Ayar <bugra.ayar@logiwa.com>
2024-02-10 12:16:51 +0300

    vertical stepper removed

--- src/app/store/create/page.tsx
+++ src/app/store/create/page.tsx
@@ -1,31 +1,7 @@
-'use client';
-import { VerticalStepper } from '@/components/common';
-import { DatePickerInput } from '@/components/inputs';
-import React, { useState } from 'react';
-
 export default function AddNewStore() {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const [startDate, setStartDate] = useState(new Date());
-
-  const stepperOptions = ['General', 'Address', 'Confirmation'];
-
-  const stepperItemClick = (index: number) => {
-    setCurrentIndex(index);
-  };
-
   return (
     <>
-      <div className="flex h-full w-full">
-        <div className="w-1/5 bg-gray-200 flex justify-center shadow-lg">
-          <VerticalStepper options={stepperOptions} onItemClick={stepperItemClick} currentIndex={currentIndex} />
-        </div>
-
-        <div className="w-4/5">
-          <div className="grid gap-6 my-6 md:grid-cols-2 m-5">
-            <DatePickerInput label="Start Date" id="dateDate" placeholder="Select Start Date" value={startDate} onChange={setStartDate} />
-          </div>
-        </div>
-      </div>
+      <div className="flex h-full w-full">Buğra</div>
     </>
   );
 }
--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,4 +1,3 @@
 import Sidebar from './sidebar/sidebar';
-import VerticalStepper from './verticalStepper';
 
-export { VerticalStepper, Sidebar };
+export { Sidebar };
