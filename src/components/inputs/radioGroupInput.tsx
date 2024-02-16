c93bb676b3f6b35fbace32d2daf087c8dfdbbab0
Buğra Mert Ayar <bugra.ayar@logiwa.com>
2024-02-16 13:21:13 +0300

    radiogroup id bug fixed

--- src/components/inputs/radioGroupInput.tsx
+++ src/components/inputs/radioGroupInput.tsx
@@ -17,7 +17,7 @@ export default function RadioGroupInput({ options, value, id, onChange }: RadioG
     <>
       <Radio.Group buttonStyle="solid" value={value} onChange={onChange} size="large">
         {options.map((option) => (
-          <Radio.Button style={{ width: `${100 / options.length}%`, textAlign: 'center' }} key={option.value} value={option.value}>
+          <Radio.Button id={id} style={{ width: `${100 / options.length}%`, textAlign: 'center' }} key={option.value} value={option.value}>
             {option.label}
           </Radio.Button>
         ))}
